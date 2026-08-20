16481675b65f88a1b908503b5965d76b70980f51
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2026-08-19 21:29:31 -1000

    Add: top-k-elements-in-list - submission-20

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-20.py
@@ -0,0 +1,17 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        res = defaultdict(list)
+        freq = defaultdict(int)
+        return_res = []
+        nums.sort()
+        for idx, num in enumerate(nums):
+            res[idx] = []
+            freq[num] += 1
+        for key in freq:
+            res[freq[key]].append(key)
+        for idx in range(len(nums), -1, -1):
+            if len(res[idx]) > 0:
+                for num in res[idx]:
+                    return_res.append(num)
+                    if len(return_res) == k:
+                        return return_res
