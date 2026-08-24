b338489b064841ea9353d6ba7b1fa16ee8157120
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2026-08-23 19:01:30 -1000

    Add: duplicate-integer - submission-10

---
+++ Data Structures & Algorithms/duplicate-integer/submission-10.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        freq = {}
+        for num in nums:
+            if num in freq:
+                return True
+            freq[num] = 0
+
+        return False
