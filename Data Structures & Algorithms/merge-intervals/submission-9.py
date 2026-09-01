05ee44b060fddc8ee56ec7bdd79417671cefb16f
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2026-08-31 17:26:27 -1000

    Add: merge-intervals - submission-9

---
+++ Data Structures & Algorithms/merge-intervals/submission-9.py
@@ -0,0 +1,18 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        curr = intervals[0]
+        res = []
+        for i in range(1,len(intervals)):
+            if curr[1] < intervals[i][0]:
+                res.append(curr)
+                curr = intervals[i]
+            else:
+                curr = [
+                    min(curr[0], intervals[i][0]),
+                    max(curr[1], intervals[i][1])
+                ]
+
+        res.append(curr)
+
+        return res
