263f9e7d15a49d208f053369744888e26f1ab486
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2026-08-31 18:04:14 -1000

    Add: meeting-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-2.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+        intervals.sort(key=lambda x: x.start)
+        min_heap = []
+
+        min_heap.append(intervals[0].end)
+
+        for i in range(1,len(intervals)):
+            if intervals[i].start < min_heap[0]:
+                heapq.heappush(min_heap, intervals[i].end)
+            else:
+                heapq.heappop(min_heap) 
+                heapq.heappush(min_heap, intervals[i].end)
+
+        return len(min_heap)
