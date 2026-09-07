363e6f97033e634447f269f4dc81a1433ba868a4
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2026-09-06 22:50:07 -1000

    Add: coin-change - submission-7

---
+++ Data Structures & Algorithms/coin-change/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+
+        memo = {}
+        coins.sort(reverse=True)
+        
+        def dfs(remaining):
+            if remaining == 0:
+                return 0
+            if remaining < 0:
+                return float("inf")
+
+            if remaining in memo:
+                return memo[remaining]
+            minCoins = float('inf')
+            for coin in coins:
+                res = dfs(remaining - coin)
+                if res != float('inf'):
+                    minCoins = min(minCoins, res + 1)
+            
+            memo[remaining] = minCoins
+            return minCoins
+
+        result = dfs(amount)
+        return result if result != float('inf') else -1   
